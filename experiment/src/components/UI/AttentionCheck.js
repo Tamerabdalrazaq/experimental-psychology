966f9a7dba14adba7c2a3c52d7efbe0bc495f8b0
Aver roes <tamerabdalrazaq@hotmail.com>
2023-05-03 13:11:29 +0300

    Instructions

--- experiment/src/components/UI/AttentionCheck.js
+++ experiment/src/components/UI/AttentionCheck.js
@@ -1,9 +1,4 @@
-import React, {
-   useEffect,
-   useState,
-   forwardRef,
-   useImperativeHandle,
-} from "react";
+import React, { useState, forwardRef, useImperativeHandle } from "react";
 import InstructionsView from "./InstructionsView";
 
 const AttentionCheck = forwardRef(
@@ -39,7 +34,7 @@ const AttentionCheck = forwardRef(
                         onClick={(e) => handleCheckClick(e, question)}
                      />
                      <label
-                        for={`checkbox-${index}`}
+                        htmlFor={`checkbox-${index}`}
                         style={{ color: `${!attentive && "red"}` }}
                      >
                         {question.label}
@@ -51,58 +46,5 @@ const AttentionCheck = forwardRef(
       );
    }
 );
-
-// function AttentionCheck({
-//    title,
-//    instructions,
-//    attention_questions,
-//    setChildReady,
-// }) {
-//    const [checkedNum, setCheckedNum] = useState(0);
-//    const [attentive, setAttentive] = useState(true);
-
-//    function allow_next() {
-//       const allowed = checkedNum === attention_questions.length - 1;
-//       if (allowed) return true;
-//       setAttentive(false);
-//       return false;
-//    }
-
-//    useEffect(() => {
-//       setChildReady(() => {
-//          allow_next();
-//       });
-//    }, []);
-
-//    return (
-//       <div className="attention-check">
-//          <InstructionsView title={title} instructions={instructions} />
-//          <div className="checklist">
-//             {attention_questions.map((question, index) => (
-//                <span>
-//                   <input
-//                      type={"checkbox"}
-//                      id={`checkbox-${index}`}
-//                      key={index}
-//                      onClick={(e) => {
-//                         setCheckedNum(
-//                            (prev) =>
-//                               prev +
-//                               (e.target.checked === question.checked ? 1 : -1)
-//                         );
-//                      }}
-//                   />
-//                   <label
-//                      for={`checkbox-${index}`}
-//                      style={{ color: `${!attentive && "red"}` }}
-//                   >
-//                      {question.label}
-//                   </label>
-//                </span>
-//             ))}
-//          </div>
-//       </div>
-//    );
-// }
 
 export default AttentionCheck;
